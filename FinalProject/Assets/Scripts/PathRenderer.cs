8b7f65e7cb40de4920baf2df2d24ec7eb7dc3494
Katilinaa <katilina0604@gmail.com>
2026-04-21 22:19:45 -0700

    Commented to make code more readable

--- FinalProject/Assets/Scripts/PathRenderer.cs
+++ FinalProject/Assets/Scripts/PathRenderer.cs
@@ -6,15 +6,21 @@ public class PathRenderer : MonoBehaviour
     private LineRenderer line;
     private void Awake()
     {
+        // Getting component to render our path
         line = GetComponent<LineRenderer>();
         line.positionCount = 0;
     }
 
+    // Function for drawing our path
     public void DrawPath(List<Vector3> points)
     {
+        // Update LineRenderer to match the number of points in the path
         line.positionCount = points.Count;
+
+        // Redraw the path in one call
         line.SetPositions(points.ToArray());
     }
+    // Clears path from the screen
     public void Clear()
     {
         line.positionCount = 0;
