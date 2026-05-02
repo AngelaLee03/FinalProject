49091df62eb9ee3ebed892c688fa96cbbaef7c01
AngelaLee03 <angelashinyoung@gmail.com>
2026-05-01 22:48:20 -0700

    messing around

--- FinalProject/Assets/Scripts/PathRenderer.cs
+++ FinalProject/Assets/Scripts/PathRenderer.cs
@@ -3,23 +3,43 @@ using System.Collections.Generic;
 
 public class PathRenderer : MonoBehaviour
 {
+    public Material pathMaterial;
+    public float textureLength = 1.2f;
+
     private LineRenderer line;
+
     private void Awake()
     {
         // Getting component to render our path
         line = GetComponent<LineRenderer>();
         line.positionCount = 0;
+
+        line.textureMode = LineTextureMode.Tile;
+        line.startColor = Color.white;
+        line.endColor = Color.white;
+        line.widthMultiplier = 0.65f;
+
+        if (pathMaterial != null)
+        {
+            line.material = pathMaterial;
+        }
     }
 
     // Function for drawing our path
     public void DrawPath(List<Vector3> points)
     {
-        // Update LineRenderer to match the number of points in the path
-        line.positionCount = points.Count;
+        Vector3[] adjusted = new Vector3[points.Count];
 
-        // Redraw the path in one call
-        line.SetPositions(points.ToArray());
+        for (int i = 0; i < points.Count; i++)
+        {
+            adjusted[i] = points[i] + Vector3.up * 0.05f;
+        }
+
+        line.positionCount = adjusted.Length;
+        line.SetPositions(adjusted);
+
     }
+
     // Clears path from the screen
     public void Clear()
     {
